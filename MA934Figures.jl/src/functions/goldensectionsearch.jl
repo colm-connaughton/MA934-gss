1f298dcac427a76ee640b0f5eb04c846d92af454
Colm Connaughton <colm.connaughton@zonlab.com>
2019-10-20 08:25:48 +0100

    Adding check to gss() function to ensure the initial bracketing triple has the right shape. Correcting all the annoying pyploy deprecation errors in the functions that produce the diagrams.

--- MA934Figures.jl/src/functions/goldensectionsearch.jl
+++ MA934Figures.jl/src/functions/goldensectionsearch.jl
@@ -1,11 +1,16 @@
 function draw_interval_arrow(ax, x1, x2, y, text, xoffset, yoffset, fs)
     x3 = 0.5*(x1+x2)
     gap = 0.08
-    ax[:annotate]("", xy=(x1, y), xycoords="data", xytext=(x3-gap/2, y), textcoords="data",
+    #ax[:annotate]("", xy=(x1, y), xycoords="data", xytext=(x3-gap/2, y), textcoords="data",
+    #    arrowprops=Dict("facecolor"=>"black", "arrowstyle"=>"->", "linewidth"=>2))
+    #ax[:annotate]("", xy=(x3+gap/2, y), xycoords="data", xytext=(x2, y), textcoords="data",
+    #    arrowprops=Dict("facecolor"=>"black", "arrowstyle"=>"<-", "linewidth"=>2))
+    #ax[:annotate](text, xy=(x3+xoffset, y+yoffset), xycoords="data", fontsize=fs)
+    ax.annotate("", xy=(x1, y), xycoords="data", xytext=(x3-gap/2, y), textcoords="data",
         arrowprops=Dict("facecolor"=>"black", "arrowstyle"=>"->", "linewidth"=>2))
-    ax[:annotate]("", xy=(x3+gap/2, y), xycoords="data", xytext=(x2, y), textcoords="data",
+    ax.annotate("", xy=(x3+gap/2, y), xycoords="data", xytext=(x2, y), textcoords="data",
         arrowprops=Dict("facecolor"=>"black", "arrowstyle"=>"<-", "linewidth"=>2))
-    ax[:annotate](text, xy=(x3+xoffset, y+yoffset), xycoords="data", fontsize=fs)
+    ax.annotate(text, xy=(x3+xoffset, y+yoffset), xycoords="data", fontsize=fs)
 end
 
 
@@ -19,10 +24,10 @@ function gssfigure1()
     
     
     fs = 15
-    ax1[:set_xlim]([-0.1, 1.1])
-    ax1[:set_ylim]([-0.1, 0.1])
-    ax1[:set_xticks]([])
-    ax1[:set_yticks]([])
+    ax1.set_xlim([-0.1, 1.1])
+    ax1.set_ylim([-0.1, 0.1])
+    ax1.set_xticks([])
+    ax1.set_yticks([])
     
     # Draw first plot
     c = a + ϕ*(b-a)
@@ -30,16 +35,16 @@ function gssfigure1()
     X = [a, c, b, x]
     Y = [0.0, 0.0 , 0.0, 0.0]
     col = [:green, :green , :green, :red]
-    ax1[:scatter](X,Y, s=30, marker="o", c=col)
+    ax1.scatter(X,Y, s=30, marker="o", c=col)
     
     # Remove the spines
-    ax1[:spines]["top"][:set_visible](false)
-    ax1[:spines]["bottom"][:set_visible](false)
-    ax1[:spines]["left"][:set_visible](false)
-    ax1[:spines]["right"][:set_visible](false)
+    ax1.spines["top"].set_visible(false)
+    ax1.spines["bottom"].set_visible(false)
+    ax1.spines["left"].set_visible(false)
+    ax1.spines["right"].set_visible(false)
     
     # Draw the x axis
-    ax1[:annotate]("", xy=(-0.1, 0.0), xycoords="data", xytext=(1.1, 0.0), textcoords="data",
+    ax1.annotate("", xy=(-0.1, 0.0), xycoords="data", xytext=(1.1, 0.0), textcoords="data",
         arrowprops=Dict("arrowstyle"=>"-", "linewidth"=>2))
     
     # Add labels
@@ -55,7 +60,7 @@ function gssfigure1()
         (0.5*(x+b) - 0.15, -0.03+yoffset,"Case 1(B)")
     ]
     for item in ann
-        ax1[:annotate](item[3], xy=(item[1], item[2]), xycoords="data", textcoords="data", fontsize=fs)
+        ax1.annotate(item[3], xy=(item[1], item[2]), xycoords="data", textcoords="data", fontsize=fs)
     end
     # Add interval arrows
     yoffset = -0.06
@@ -64,7 +69,7 @@ function gssfigure1()
     draw_interval_arrow(ax1, x, c, yoffset-0.02, "r", -0.02, -0.005, fs)
     
     # Draw the sub-interval brackets
-    ax1[:annotate]("",
+    ax1.annotate("",
                 xy=(a, 0.01), xycoords="data",
                 xytext=(c, 0.01), textcoords="data",
                 arrowprops=Dict("arrowstyle"=>"-",
@@ -75,7 +80,7 @@ function gssfigure1()
                                 "connectionstyle"=>"bar,fraction=0.1",
                                 )
                 )
-    ax1[:annotate]("",
+    ax1.annotate("",
                 xy=(x, -0.025), xycoords="data",
                 xytext=(b, -0.025), textcoords="data",
                 arrowprops=Dict("arrowstyle"=>"-",
@@ -94,16 +99,16 @@ function gssfigure1()
     X = [a, c, b, x]
     Y = [0.0, 0.0 , 0.0, 0.0]
     col = [:green, :green , :green, :red]
-    ax2[:scatter](X,Y, s=30, marker="o", c=col)
+    ax2.scatter(X,Y, s=30, marker="o", c=col)
     
     # Remove the spines
-    ax2[:spines]["top"][:set_visible](false)
-    ax2[:spines]["bottom"][:set_visible](false)
-    ax2[:spines]["left"][:set_visible](false)
-    ax2[:spines]["right"][:set_visible](false)
+    ax2.spines["top"].set_visible(false)
+    ax2.spines["bottom"].set_visible(false)
+    ax2.spines["left"].set_visible(false)
+    ax2.spines["right"].set_visible(false)
     
     # Draw the x axis
-    ax2[:annotate]("", xy=(-0.1, 0.0), xycoords="data", xytext=(1.1, 0.0), textcoords="data",
+    ax2.annotate("", xy=(-0.1, 0.0), xycoords="data", xytext=(1.1, 0.0), textcoords="data",
         arrowprops=Dict("arrowstyle"=>"-", "linewidth"=>2))
     
     # Add labels
@@ -119,7 +124,7 @@ function gssfigure1()
         (0.5*(c+b) - 0.15, -0.03+yoffset,"Case 2(B)")
     ]
     for item in ann
-        ax2[:annotate](item[3], xy=(item[1], item[2]), xycoords="data", textcoords="data", fontsize=fs)
+        ax2.annotate(item[3], xy=(item[1], item[2]), xycoords="data", textcoords="data", fontsize=fs)
     end
     # Add interval arrows
     yoffset = -0.06
@@ -128,7 +133,7 @@ function gssfigure1()
     draw_interval_arrow(ax2, c, x, yoffset-0.02, "r", -0.02, -0.005, fs)
     
      # Draw the sub-interval brackets
-    ax2[:annotate]("",
+    ax2.annotate("",
                 xy=(a, 0.01), xycoords="data",
                 xytext=(x, 0.01), textcoords="data",
                 arrowprops=Dict("arrowstyle"=>"-",
@@ -139,7 +144,7 @@ function gssfigure1()
                                 "connectionstyle"=>"bar,fraction=0.1",
                                 )
                 )
-    ax2[:annotate]("",
+    ax2.annotate("",
                 xy=(c, -0.025), xycoords="data",
                 xytext=(b, -0.025), textcoords="data",
                 arrowprops=Dict("arrowstyle"=>"-",
@@ -163,10 +168,10 @@ function gssfigure2()
     
     
     fs = 15
-    ax1[:set_xlim]([-0.1, 1.1])
-    ax1[:set_ylim]([-0.1, 0.1])
-    ax1[:set_xticks]([])
-    ax1[:set_yticks]([])
+    ax1.set_xlim([-0.1, 1.1])
+    ax1.set_ylim([-0.1, 0.1])
+    ax1.set_xticks([])
+    ax1.set_yticks([])
     
     # Draw first plot
     c = a + ϕ*(b-a)
@@ -174,16 +179,16 @@ function gssfigure2()
     X = [a, c, b, x]
     Y = [0.0, 0.0 , 0.0, 0.0]
     col = [:green, :green , :green, :red]
-    ax1[:scatter](X,Y, s=30, marker="o", c=col)
+    ax1.scatter(X,Y, s=30, marker="o", c=col)
     
     # Remove the spines
-    ax1[:spines]["top"][:set_visible](false)
-    ax1[:spines]["bottom"][:set_visible](false)
-    ax1[:spines]["left"][:set_visible](false)
-    ax1[:spines]["right"][:set_visible](false)
+    ax1.spines["top"].set_visible(false)
+    ax1.spines["bottom"].set_visible(false)
+    ax1.spines["left"].set_visible(false)
+    ax1.spines["right"].set_visible(false)
     
     # Draw the x axis
-    ax1[:annotate]("", xy=(-0.1, 0.0), xycoords="data", xytext=(1.1, 0.0), textcoords="data",
+    ax1.annotate("", xy=(-0.1, 0.0), xycoords="data", xytext=(1.1, 0.0), textcoords="data",
         arrowprops=Dict("arrowstyle"=>"-", "linewidth"=>2))
     
     # Add labels
@@ -198,12 +203,12 @@ function gssfigure2()
         (0.5*(x+b) - 0.15, -0.03+yoffset,"Case 2")
     ]
     for item in ann
-        ax1[:annotate](item[3], xy=(item[1], item[2]), xycoords="data", textcoords="data", fontsize=fs)
+        ax1.annotate(item[3], xy=(item[1], item[2]), xycoords="data", textcoords="data", fontsize=fs)
     end
     
     
     # Draw the sub-interval brackets
-    ax1[:annotate]("",
+    ax1.annotate("",
                 xy=(a, 0.01), xycoords="data",
                 xytext=(c, 0.01), textcoords="data",
                 arrowprops=Dict("arrowstyle"=>"-",
@@ -214,7 +219,7 @@ function gssfigure2()
                                 "connectionstyle"=>"bar,fraction=0.1",
                                 )
                 )
-    ax1[:annotate]("",
+    ax1.annotate("",
                 xy=(x, -0.025), xycoords="data",
                 xytext=(b, -0.025), textcoords="data",
                 arrowprops=Dict("arrowstyle"=>"-",
@@ -234,19 +239,20 @@ function gssfigure3(steps, position, width)
     w = phi.^steps 
     
     fig, (ax1, ax2) = subplots(1, 2, figsize=(10,5))
-    ax1[:axhline](color="black")
-    ax1[:plot](steps, position, "ro-")
-    ax1[:set_title]("Approach to root")
-    ax1[:set_xlabel]("Number of steps, n")
-    ax1[:set_ylabel]("Estimate of root, x")
+    ax1.axhline(color="black")
+    ax1.plot(steps, position, "ro-")
+    ax1.set_title("Approach to root")
+    ax1.set_xlabel("Number of steps, n")
+    ax1.set_ylabel("Estimate of root, x")
     
     
     
-    ax2[:semilogy](steps, width, "bo-", label="Golden section search")
-    ax2[:plot](steps, w, "g-", label=L"Theoretical: $\phi^n$")
-    ax2[:set_title]("Width of bracketing interval")
-    ax2[:set_xlabel]("Number of steps, n")
-    ax2[:set_ylabel]("b-a")
+    
+    ax2.semilogy(steps, width, "bo-", label="Golden section search")
+    ax2.plot(steps, w, "g-", label=L"Theoretical: $\phi^n$")
+    ax2.set_title("Width of bracketing interval")
+    ax2.set_xlabel("Number of steps, n")
+    ax2.set_ylabel("b-a")
     legend()
     
     return fig, (ax1, ax2)
